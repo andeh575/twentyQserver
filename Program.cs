9afaf01e8561d14d57d2fbb9d6a7e3120e9258f4
Andrew Graham <andrew.t.graham@live.com>
2014-05-15 21:11:07 -0700

    initial commit
    Server side implementation of twentyQ

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace twentyQserver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 14000;
+            server server = new server(port);
+
+            server.start();
+
+            Console.WriteLine("Started twentyQ server on port number: {0}", port);
+            Console.WriteLine("Press [q] to stop the server");
+
+            while(true)
+            {
+                ConsoleKeyInfo c = Console.ReadKey(true);
+
+                if(c.Key == ConsoleKey.Q)
+                {
+                    Console.WriteLine("Closing server...");
+                    server.stop();
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ server.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace twentyQserver
+{
+    class server
+    {
+        public int Port { get; set; }
+        public TcpListener listener;
+        private Thread listenerThread;
+
+        public server (int port)
+        {
+            this.Port = port;
+        }
+        
+        public void start ()
+        {
+            listener = new TcpListener(new IPEndPoint(IPAddress.Any, Port));
+            listener.Start();
+            listenerThread = new Thread(new ThreadStart(Listener));
+            listenerThread.Start();
+        }
+
+        public void stop()
+        {
+            listener.Stop();
+            listenerThread.Abort();
+        }
+        
+        private void Listener()
+        {
+            while(true)
+            {
+                try
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    new Thread(new ThreadStart(() => HandleClient(client))).Start();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error in Listener Thread:");
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+        }
+
+        private void HandleClient(TcpClient client)
+        {
+            
+        }
+    }
+}
